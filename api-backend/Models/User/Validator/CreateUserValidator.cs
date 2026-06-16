42d1c909578578c2385a5a6cb415e34d5e7772f4
Leonardo Braga <leonardolopbraga@gmail.com>
2026-06-16 12:42:43 -0300

    Fix: LowerCase para role de criação de conta.

--- api-backend/Models/User/Validator/CreateUserValidator.cs
+++ api-backend/Models/User/Validator/CreateUserValidator.cs
@@ -5,8 +5,10 @@ public class CreateUserValidator : AbstractValidator<CreateUserDto>
     public CreateUserValidator()
     {
         RuleFor(x => x.Username).NotEmpty().WithMessage("O nome de usuário é obrigatório.");
+        
         RuleFor(x => x.Password).NotEmpty().MinimumLength(8).WithMessage("A senha deve ter no mínimo 8 caracteres.");
-        RuleFor(x => x.Role).Must(r => r == "Admin" || r == "Lider" || r == "Analista")
+
+        RuleFor(x => x.Role).Must(r => r.ToLower() == "admin" || r.ToLower() == "lider" || r.ToLower() == "analista")
             .WithMessage("O nível de acesso deve ser Admin, Lider ou Analista.");
     }
 }
